ce45a1f5f233cd690263c2bff2c98b2cbb41cb77
Samuel Svensäter <peterodd@gmail.com>
2016-02-15 16:34:58 +0100

    initial commit

---
+++ lab2/upload/Registration/REG21.m
@@ -0,0 +1,148 @@
+close all;
+clear all;
+clc
+
+
+%Load images
+
+I{1}=rgb2gray(imread('01.png'));
+I{2}=rgb2gray(imread('02.png'));
+I{3}=rgb2gray(imread('03.png'));
+
+I_padded{1} = padarray(imread('01.png'),size(I{1}),0);
+
+
+figure;
+for i=1:3
+    subplot(1,3,i);
+    imshow(I{i});
+    
+end;
+%% Generate the edge image using Canny
+
+
+t1 = 0.1;
+t2 = 0.2;
+sigma = 0.7;
+se=strel('disk',2);
+for i=1:3
+a_edge{i} = imdilate(edge(I{i},'canny',[t1 t2],sigma),se);
+%a_edge{i} =edge(I{i},'canny',[t1 t2],sigma);
+
+end;
+
+figure;
+for i=1:3
+subplot(1,3,i)
+imshow(a_edge{i},[]);
+title('Canny result from test image')
+end;
+
+%% Calculate the distance transform of the original edge image (the base)
+figure;
+base = bwdist(a_edge{1},'euclidean');
+
+imshow(base,[]);
+title('Base image')
+%% Specify the directions that will be used for 
+
+step =5;
+
+translation_directions = [0    -step; 
+                          -step  0;
+                           0     step; 
+                           step  0];
+                       
+%% Specify rotation that will be used.
+angle=1;
+rotation_directions = [angle -angle]; 
+                       
+                       
+%% Now pad the images to make room to manouver
+offset=[100 100;
+        0 0];
+
+movement=[0 0 0;
+          0 0 0];
+      base2 = padarray(base,size(base),max(base(:)));
+for im_nr=1:2
+
+    floating = padarray(a_edge{im_nr+1},size(a_edge{im_nr+1}),0);
+
+
+    %% Finally do the chamermatching
+
+
+    last_score = inf; % Keep track of last positional score
+
+    stop = false; % Stop criterion
+    backwards = 0; % The direction which we came from
+    accumulated_scores = []; % For plotting reasons
+    counter = 1;
+
+    %Initial position
+    floating = circshift(floating,offset(im_nr,:));
+    
+    
+    while ~stop
+    scores = zeros([4 2]); % To save the scores
+
+
+        for i=1:size(translation_directions,1)
+
+            %Two different rotations
+            tmp_image1 = imrotate(floating,rotation_directions(1),'crop');
+            tmp_image2 = imrotate(floating,rotation_directions(2),'crop');
+
+
+            %Translation with two rotations
+            tmp_image1 = circshift(tmp_image1,translation_directions( i,:));
+            tmp_image2 = circshift(tmp_image2,translation_directions( i,:));
+
+            %Find sums of distance
+            scores(i,1)=sum(base2(logical(tmp_image1)));
+            scores(i,2)=sum(base2(logical(tmp_image2)));
+
+        end;
+            disp(scores);
+
+         [best_score,dir]=min(scores(:));
+            disp(best_score);
+
+         if best_score <1%> last_score || dir == backwards
+            stop = true;
+            accumulated_scores(end+1) = best_score; %#ok
+         else
+             if(dir>=1 && dir<=4)
+                 rot_angle=rotation_directions(1);
+
+             elseif(dir>4 && dir<=8)
+                 rot_angle=rotation_directions(2);
+             end;
+
+            movement(im_nr,1)=movement(1,1)+rot_angle;
+            floating= imrotate(floating,rot_angle,'crop');
+            disp(['Rotating ' num2str(rot_angle) ' degrees']);
+
+            movement(im_nr,2:3)=movement(1,2:3)+[translation_directions(mod(dir-1,4)+1,1) translation_directions(mod(dir-1,4)+1,2)];
+            floating = imtranslate(floating,translation_directions(mod(dir-1,4)+1,1),translation_directions(mod(dir-1,4)+1,2));
+            disp(['Translating x(' num2str(translation_directions(mod(dir-1,4)+1,2)) ') y(' num2str(translation_directions(mod(dir-1,4)+1,1)) ')' ]);
+            backwards = mod(dir+1,8)+1;
+            last_score = best_score;
+            accumulated_scores(end+1) = best_score; %#ok
+        end
+
+        figure(222);
+        imshow(edgeRGBoverlay(I_padded{1},floating,'red'),[])
+        title(['Floating image position for iteration ' num2str(counter)])
+        pause(0.05);
+
+        if(counter>20)
+            break
+        end;
+
+        counter = counter + 1;
+    end
+    figure;
+    plot(accumulated_scores,'r');
+end;
---
+++ lab2/upload/Registration/chamfer.m
@@ -0,0 +1,119 @@
+clc; clear; close all;
+figure('units','normalized','outerposition',[0 0 1 1]); %full screen figure
+
+%% load images
+base = imread('01.png');
+img2 = imread('02.png');
+img3 = imread('03.png');
+orig = imread('01.png');
+
+%% display the three images
+subplot(331);
+imshow(base);
+title('Image 01');
+subplot(332);
+imshow(img2);
+title('Image 02');
+subplot(333);
+imshow(img3);
+title('Image 03');
+
+
+%% show canny of base image
+t1 = 0.1;
+t2 = 0.2;
+sigma = 0.8;
+base_gray = rgb2gray(base); %convert to grayscale
+base_edge = edge(base_gray, 'canny', [t1 t2], sigma); %canny edges
+subplot(334);
+imshow(base_edge);
+title('Canny of base (01) image');
+
+
+%% canny for img2
+img2_gray = rgb2gray(img2);
+img2_edge = edge(img2_gray, 'canny', [t1 t2], sigma);
+
+
+%% canny for img3
+img3_gray = rgb2gray(img3);
+img3_edge = edge(img3_gray, 'canny', [t1 t2], sigma);
+
+
+%% calculate DT of the original edge image (base)
+baseDT = bwdist(base_edge, 'euclidean');
+subplot(335);
+imshow(baseDT,[]);
+title('DT of base image');
+
+%% specify the directions that will be used for
+step = 3;
+translation_directions = [
+                            0      -step;
+                            -step  0;
+                            0      step;
+                            step   0 
+                          ];
+
+%% specify the rotations that will be used for
+rot_size = 20;
+number_of_rots = 10;
+rotation_directions = -rot_size:rot_size/number_of_rots:rot_size;
+
+%% now pad the images to make room to manouver
+baseDT = padarray(baseDT, size(baseDT), max(baseDT(:)));
+img2_edge = padarray(img2_edge, size(img2_edge), 0);
+
+
+%% chamfermatching!
+last_score = inf;
+stop = false;
+backwards = 0;
+accumulated_scores = [];
+counter = 1;
+len = length(orig(:,1,1));
+visitedCoords = zeros(len,len,rot_size*number_of_rots);
+
+while ~stop
+    top_image = img2_edge;
+    scores = zeros(size(translation_directions, 1), 1);
+    
+    for i = 1 : size(translation_directions, 1)
+        tmp_image = circshift(img2_edge, translation_directions(i,:));
+        
+        scores(i) = sum(baseDT(logical(tmp_image)));
+    end
+    
+    [best_score, dir] = min(scores);
+
+    if best_score > last_score %|| dir == backwards
+        stop = true;
+        accumulated_scores(end+1) = best_score;
+    else
+        img2_edge = imrotate(img2_edge,rotation_directions(x),'bilinear','crop');
+        img2_edge = imtranslate(img2_edge, translation_directions(dir, 1), translation_directions(dir, 2));
+        backwards = mod(dir+1, 4) + 1;
+        last_score = best_score;
+        accumulated_scores(end+1) = best_score;
+    end
+   
+    
+    subplot(336);
+    imshow(edgeRGBoverlay(baseDT, img2_edge,'red'),[])
+    title(['Floating image position for iteration ' num2str(counter)])
+    counter = counter + 1;
+    
+
+end
+
+subplot(337);
+plot(accumulated_scores, 'r');
+title('Positional scores');
+ylabel('Scores');
+xlabel('Iterations');
+
+%% show merged result
+subplot(338);
+imshow(meanRGB(orig,img2));
+title('Images 01, 02 & 03 merged');
+
